ed0d210ddf92232d9f4509109eb5a73927c613c3
JustinAndrade <j.andrade.john@gmail.com>
2019-06-25 18:23:33 -0400

    skeleton created

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,6 @@
-// Test away!
+import react from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+
+import Controls from './Controls.js';
